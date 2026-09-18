0ac49310df4777e7999f86eab6ff633222e083ce
ABHIJEETSAWANT-11 <abhijeetsawant99709@gmail.com>
2026-09-18 20:37:26 +0530

    Add logic to find two indices for target sum
    Implement nested loop to find two indices that sum to target.

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,6 +4,7 @@ public:
     int sum=0;
     for(int i=0;i<nums.size();i++){
         for(int j=i+1;j<nums.size();j++){
+            
            sum=nums[i]+nums[j];
             
             if(sum==target ){

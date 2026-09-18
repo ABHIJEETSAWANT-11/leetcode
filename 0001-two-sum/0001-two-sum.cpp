52e820af3c3c9e7099899be77af889b9ce354b54
ABHIJEETSAWANT-11 <abhijeetsawant99709@gmail.com>
2026-09-18 20:33:27 +0530

    Correct two-sum logic to return indices
    Fix the two-sum function to return indices when sum matches target.

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,6 +5,7 @@ public:
     for(int i=0;i<nums.size();i++){
         for(int j=i+1;j<nums.size();j++){
            sum=nums[i]+nums[j];
+            
            
            
             if(sum==target ){

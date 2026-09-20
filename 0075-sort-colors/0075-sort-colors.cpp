a8a11df73e7cf99a2f5ceb0ad059eff0e675c2b3
ABHIJEETSAWANT-11 <abhijeetsawant99709@gmail.com>
2026-09-20 21:14:58 +0530

    Update 0075-sort-colors.cpp

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -9,16 +9,11 @@ public:
             else blue.push_back(2);
         }
         
-
-
         
         nums.clear();
         nums.insert(nums.end(), red.begin(), red.end());
         nums.insert(nums.end(), white.begin(), white.end());
         nums.insert(nums.end(), blue.begin(), blue.end());
 
-
-
-        
     }
 };

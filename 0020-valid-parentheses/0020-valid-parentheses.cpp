c3173f9e4fa19bfadbc4a20c1897abc2e1a7955b
ABHIJEETSAWANT-11 <sawantabhijeet2005@gmail.com>
2026-09-23 23:42:38 +0530

    Update 0020-valid-parentheses.cpp

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,8 +4,8 @@ public:
 
         while (true) {
             int n = s.size();
-
-            for (int i = 0; i < n - 1; i++) {
+ for (int i = 0; i < n - 1; i++)
+            {
                 if ((s[i] == '(' && s[i + 1] == ')') ||
                     (s[i] == '{' && s[i + 1] == '}') ||
                     (s[i] == '[' && s[i + 1] == ']')) {
